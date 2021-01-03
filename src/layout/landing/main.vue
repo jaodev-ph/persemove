215e9a4c45a227edb1c78a40b12703d2f1699f50
jao <josephjaojoco@gmail.com>
2021-01-04 03:16:26 +0800

    building

--- src/layout/landing/main.vue
+++ src/layout/landing/main.vue
@@ -1,88 +1,83 @@
 <template>
-  <div class="feature">
-    <div class="feature-header">
-      <h3>Persemove</h3>
-      <h2><strong>A better way to send money</strong></h2>
-      <h5 class="text-newline">
-        Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
-        voluptatum cupiditate veritatis in accusamus quisquam.
-      </h5>
+  <div>
+    <div class="company-banner">
+      <img src="@/assets/icons/banner.jpg" alt="" />
     </div>
-    <div class="feature-body">
-      <div class="feature-item-wrapper">
-        <div class="row">
-          <div class="col-md-6">
-            <div class="feature-body-item">
-              <div class="f-icon">
-                <b-icon
-                  icon="exclamation-circle-fill"
-                  variant="success"
-                ></b-icon>
-              </div>
-              <div>
-                <h6>Competitive exchange rates</h6>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="feature-body-item">
-              <div class="f-icon">
-                <b-icon
-                  icon="exclamation-circle-fill"
-                  variant="success"
-                ></b-icon>
-              </div>
-              <div>
-                <h6>Competitive exchange rates</h6>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </p>
-              </div>
-            </div>
-          </div>
+    <div class="feature">
+      <div class="container">
+        <div class="feature-header">
+          <h5>PERSEMOVE</h5>
+          <h2><strong>A better way to send money</strong></h2>
+          <h5 class="text-newline">
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam
+            voluptatum cupiditate veritatis in accusamus quisquam.
+          </h5>
         </div>
-        <br />
-        <div class="row">
-          <div class="col-md-6">
-            <div class="feature-body-item">
-              <div class="f-icon">
-                <b-icon
-                  icon="exclamation-circle-fill"
-                  variant="success"
-                ></b-icon>
+        <div class="feature-body">
+          <div class="feature-item-wrapper">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="feature-body-item">
+                  <div class="f-icon">
+                    <a href=""><img src="@/assets/icons/fb.svg" alt="" /></a>
+                  </div>
+                  <div class="fbi-description">
+                    <h6>Competitive exchange rates</h6>
+                    <p>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Maiores impedit perferendis suscipit eaque, iste dolor
+                      cupiditate blanditiis ratione.
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <h6>Competitive exchange rates</h6>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </p>
+              <div class="col-md-6">
+                <div class="feature-body-item">
+                  <div class="f-icon">
+                    <a href=""><img src="@/assets/icons/fb.svg" alt="" /></a>
+                  </div>
+                  <div>
+                    <h6>No hidden fees</h6>
+                    <p>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Maiores impedit perferendis suscipit eaque, iste dolor
+                      cupiditate blanditiis ratione.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-md-6">
-            <div class="feature-body-item">
-              <div class="f-icon">
-                <b-icon
-                  icon="exclamation-circle-fill"
-                  variant="success"
-                ></b-icon>
+            <br />
+            <div class="row">
+              <div class="col-md-6">
+                <div class="feature-body-item">
+                  <div class="f-icon">
+                    <a href=""><img src="@/assets/icons/fb.svg" alt="" /></a>
+                  </div>
+                  <div>
+                    <h6>Transfers are instant</h6>
+                    <p>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Maiores impedit perferendis suscipit eaque, iste dolor
+                      cupiditate blanditiis ratione.
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div>
-                <h6>Competitive exchange rates</h6>
-                <p>
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Maiores impedit perferendis suscipit eaque, iste dolor
-                  cupiditate blanditiis ratione.
-                </p>
+              <div class="col-md-6">
+                <div class="feature-body-item">
+                  <div class="f-icon">
+                    <a href=""><img src="@/assets/icons/fb.svg" alt="" /></a>
+                  </div>
+                  <div>
+                    <h6>Mobile notifications</h6>
+                    <p>
+                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                      Maiores impedit perferendis suscipit eaque, iste dolor
+                      cupiditate blanditiis ratione.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/layout/landing/navbar.vue
+++ src/layout/landing/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="info">
+  <b-navbar toggleable="lg" type="dark">
     <b-navbar-brand href="#">Persemove </b-navbar-brand>
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
